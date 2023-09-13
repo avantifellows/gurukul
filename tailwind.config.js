986ab0c969fdb26e1c82a6c266a3813cd7315dd3
Bahugunajii <amanbahuguna009@gmail.com>
2023-09-13 14:50:24 +0530

    removed secondary color from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: "#008181",
-        secondary: "#b9cdda"
+        primary: "#008181"
       },
     },
   },
