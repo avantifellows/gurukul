233244a639758b0cdccf9045d3196bf0f80517d8
Bahugunajii <amanbahuguna009@gmail.com>
2023-09-25 13:04:39 +0530

    added AuthProvider to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { AuthProvider } from './AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body className={inter.className}>{children}</body>
+      </AuthProvider>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+
+"use client"
+import { useAuth } from "./AuthContext";
+
 export default function Home() {
+  const { loggedIn, userId } = useAuth();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Welcome To Gurukul
+      {loggedIn ? (
+        <p>Welcom to Gurukul</p>
+      ) : (
+        <p>User not logged in</p>
+      )}
     </main>
   );
 }
