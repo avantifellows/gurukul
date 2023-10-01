b935a01638a360416f9ca976fb19de944c900bf3
Bahugunajii <amanbahuguna009@gmail.com>
2023-10-01 13:29:28 +0530

    suggested changes

--- components/BottomNavigationBar.tsx
+++ components/BottomNavigationBar.tsx
@@ -1,23 +1,14 @@
 "use client"
 
-import { Icon } from '@iconify/react';
 import { usePathname } from 'next/navigation';
 import AvantiLogo from '../assets/avanti_logo.png';
 import Image from 'next/image';
 import NavLink from './NavLink';
+import { MdOutlineLibraryBooks, MdLibraryBooks } from 'react-icons/md';
+import { RiBarChart2Fill, RiBarChart2Line } from 'react-icons/ri';
 
 const BottomNavigationBar = () => {
   const pathname = usePathname();
-
-  const iconStyle = {
-    fontSize: '32px',
-    color: 'black',
-  };
-
-  const activeIconStyle = {
-    fontSize: '32px',
-    color: '#008181',
-  };
 
   const isActive = (path: string) => {
     return pathname === path;
@@ -26,10 +17,11 @@ const BottomNavigationBar = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 text-xs md:text-lg pl-16 pr-16 bg-white pb-4 pt-2 flex justify-between border-t-2 items-center shadow-2xl shadow-black">
       <NavLink href="/library" active={isActive('/library')}>
-        <Icon
-          icon="ic:round-library-books"
-          style={isActive('/library') ? activeIconStyle : iconStyle}
-        />
+        {isActive('/library') ? (
+          <MdLibraryBooks className='h-8 w-8 fill-primary' />
+        ) : (
+          <MdOutlineLibraryBooks className='h-8 w-8' />
+        )}
         Library
       </NavLink>
       <NavLink href="/" active={isActive('/')}>
@@ -37,10 +29,11 @@ const BottomNavigationBar = () => {
         <span className="pt-8">Home</span>
       </NavLink>
       <NavLink href="/reports" active={isActive('/reports')}>
-        <Icon
-          icon="uil:graph-bar"
-          style={isActive('/reports') ? activeIconStyle : iconStyle}
-        />
+        {isActive('/reports') ? (
+          <RiBarChart2Fill className='h-8 w-8 fill-primary' />
+        ) : (
+          <RiBarChart2Line className='h-8 w-8' />
+        )}
         Report
       </NavLink>
     </div>
