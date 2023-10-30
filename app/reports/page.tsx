5757af4e721dc0e840fba96491d997a654071984
Bahugunajii <amanbahuguna009@gmail.com>
2023-10-30 18:42:41 +0530

    ci fixes

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -6,26 +6,6 @@ import { Suspense } from "react";
 import Loading from "../loading";
 import BottomNavigationBar from "@/components/BottomNavigationBar";
 
-export async function getData() {
-    const apiKey = process.env.AF_REPORTS_DB_API_KEY;
-    const studentId = process.env.NEXT_PUBLIC_STUDENT_ID;
-    const url = `${process.env.AF_REPORTS_URL}/student_reports/${studentId}?format=json`;
-
-    try {
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization: `Bearer ${apiKey}`,
-            },
-        });
-        const responseData = await response.json();
-        return responseData;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 export default async function ReportsPage() {
     return (
         <div>
