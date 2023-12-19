dbc07f510a8cef6780463f0bd16409a8199b6d26
Bahugunajii <amanbahuguna009@gmail.com>
2023-12-19 12:50:09 +0530

    added explanation for the commented code

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
+    /* Date: 19-12-2023 
+     The async redirects block below was originally implemented to redirect users from the home URL to the library page. For the demo, we've decided to disable this behavior by commenting out the code.
+    */
 
     // async redirects() {
     //     return [
