17538f159e16f43da77215c28fc2dcad1869f28b
Bahugunajii <amanbahuguna009@gmail.com>
2024-01-25 13:48:15 +0530

    moved secrets to env

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,14 +1,16 @@
+"use server"
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA94zc1GEle4hhh6pPe1GMoublv6Kae60o",
-    authDomain: "staging-portal-7df0f.firebaseapp.com",
-    projectId: "staging-portal-7df0f",
-    storageBucket: "staging-portal-7df0f.appspot.com",
-    messagingSenderId: "631798984229",
-    appId: "1:631798984229:web:ff327cceff980940f219cc",
-    measurementId: "G-QS80G2CVRN"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
