7e7b735291b1569022d8bfd7de0ee7653d6210d2
Bahugunajii <amanbahuguna009@gmail.com>
2024-03-14 17:45:20 +0530

    fixed button alignment inside a card

--- app/page.tsx
+++ app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
                         {formatTime(data.end_time)}
                       </p>
                     </div>
-                    <div className="bg-white rounded-lg shadow-lg min-h-24 h-auto py-6 relative w-full flex flex-row justify-between mr-4">
+                    <div className="bg-white rounded-lg shadow-lg min-h-24 h-auto py-6 relative w-full flex flex-row justify-between mr-4 items-center">
                       <div className={`${index % 2 === 0 ? 'bg-orange-200' : 'bg-red-200'} h-full w-2 absolute left-0 top-0 rounded-s-md`}></div>
                       <div className="text-sm md:text-base font-semibold mx-6 md:mx-8">
                         <span className="font-normal pr-4">Subject:</span> {data.session.meta_data.subject ?? "Science"}
@@ -210,7 +210,7 @@ export default function Home() {
                         {formatQuizSessionTime(data.end_time)}
                       </p>
                     </div>
-                    <div className="bg-white rounded-lg shadow-lg min-h-24 h-auto py-6 relative w-full flex flex-row justify-between mr-4">
+                    <div className="bg-white rounded-lg shadow-lg min-h-24 h-auto py-6 relative w-full flex flex-row justify-between mr-4 items-center">
                       <div className={`${index % 2 === 0 ? 'bg-orange-200' : 'bg-red-200'} h-full w-2 absolute left-0 top-0  rounded-s-md`}></div>
                       <div className="text-sm md:text-base font-semibold mx-6 md:mx-8">
                         <div className="flex">
