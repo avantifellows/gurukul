d8c7ee4838aff2d16cd82d65ae6d57d779bce04d
Bahugunajii <amanbahuguna009@gmail.com>
2024-04-02 01:54:40 +0530

    fetch quiz from dbservice

--- utils/dateUtils.ts
+++ utils/dateUtils.ts
@@ -13,6 +13,9 @@ export function formatSessionTime(dateTimeStr: string) {
         }
 
         date = new Date(2000, 0, 1, hours, minutes);
+    } else if (dateTimeStr.includes('T') && dateTimeStr.includes('Z')) {
+        const isoDate = new Date(dateTimeStr);
+        date = new Date(2000, 0, 1, isoDate.getUTCHours(), isoDate.getUTCMinutes());
     } else {
         const [hours, minutes, seconds] = dateTimeStr.split(':').map(Number);
         date = new Date(2000, 0, 1, hours, minutes, seconds);
@@ -29,14 +32,28 @@ export function formatCurrentTime(dateTimeStr: string) {
     return `${hours}:${minutes}`;
 }
 
-export function formatQuizSessionTime(dateTimeStr: string) {
-    const time = new Date(`01/01/2000 ${dateTimeStr}`);
-    return time.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+export function formatQuizSessionTime(dateTimeString: string): string {
+    const dateTime = new Date(dateTimeString);
+    const hours = dateTime.getUTCHours().toString().padStart(2, '0');
+    const minutes = dateTime.getUTCMinutes().toString().padStart(2, '0');
+    const time = hours + ':' + minutes;
+    return time;
 }
 
 export function formatTime(dateTimeStr: string) {
     const time = new Date(`2000-01-01T${dateTimeStr}`);
     return time.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true });
+}
+
+export function format12HrQuizSessionTime(time: string): string {
+    const isoDate = new Date(time);
+    const hours = isoDate.getUTCHours();
+    const minutes = isoDate.getUTCMinutes();
+    const period = hours >= 12 ? 'PM' : 'AM';
+    let formattedHours = hours % 12;
+    formattedHours = formattedHours ? formattedHours : 12;
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    return `${formattedHours}:${formattedMinutes} ${period}`;
 }
 
 export function isSessionActive(endTime: string): boolean {
