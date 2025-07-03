725f22e04c5c7adff286cb6c228f37a397e633f8
Aman Bahuguna <amanbahuguna009@gmail.com>
2025-07-03 15:06:12 +0530

    updated library

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -8,6 +8,9 @@ import BottomNavigationBar from '@/components/BottomNavigationBar';
 import Image from 'next/image';
 import StethoscopeIcon from '../../assets/stethoscope.png';
 import BlueprintIcon from '../../assets/blueprint.png';
+import { MdScience } from 'react-icons/md';
+import { GiScales } from 'react-icons/gi';
+import { FaRupeeSign } from 'react-icons/fa';
 import { MixpanelTracking } from '@/services/mixpanel';
 import { MIXPANEL_EVENT } from '@/constants/config';
 import { useAuth } from '@/services/AuthContext';
@@ -25,10 +28,19 @@ const Page: React.FC = () => {
   };
 
   useEffect(() => {
-    if (selectedLibrary === 'NEET Content' || selectedLibrary === 'JEE Content') {
+    if (
+      selectedLibrary === 'NEET Content' ||
+      selectedLibrary === 'JEE Content' ||
+      selectedLibrary === 'JEE Advance Content' ||
+      selectedLibrary === 'CLAT Content' ||
+      selectedLibrary === 'CA Content'
+    ) {
       push(`/library/content?course=${selectedLibrary}`);
     }
-    if (selectedLibrary === 'NEET Classes' || selectedLibrary === 'JEE Classes') {
+    if (
+      selectedLibrary === 'NEET Classes' ||
+      selectedLibrary === 'JEE Classes'
+    ) {
       push(`/library/class?course=${selectedLibrary}`);
     }
   }, [selectedLibrary, push]);
@@ -61,7 +73,7 @@ const Page: React.FC = () => {
       )}
 
       {selectedLibrary === 'Content' && (
-        <div className="bg-white h-72">
+        <div className="bg-white pb-40">
           <div onClick={() => handleLibraryChange('NEET Content')} className="pt-4">
             <div className="bg-card rounded-md shadow-lg shadow-slate-400 h-24 mt-2 my-10 text-black flex items-center justify-start pl-4 mx-6 cursor-pointer">
               <div className="flex items-center">
@@ -80,6 +92,39 @@ const Page: React.FC = () => {
                 <div className="flex flex-col ml-4 ">
                   <h3 className="font-semibold">JEE Mains course</h3>
                   <h5 className="text-sm">Browse all the JEE courses</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div onClick={() => handleLibraryChange('JEE Advance Content')}>
+            <div className="bg-card rounded-md shadow-lg shadow-slate-400 h-24 mt-2 my-10 text-black flex items-center justify-start pl-4 mx-6 cursor-pointer">
+              <div className="flex flex-row items-center">
+                <MdScience className="w-10 h-10 text-blue-700" />
+                <div className="flex flex-col ml-4 ">
+                  <h3 className="font-semibold">JEE Advance course</h3>
+                  <h5 className="text-sm">Browse all the JEE Advance courses</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div onClick={() => handleLibraryChange('CLAT Content')}>
+            <div className="bg-card rounded-md shadow-lg shadow-slate-400 h-24 mt-2 my-10 text-black flex items-center justify-start pl-4 mx-6 cursor-pointer">
+              <div className="flex flex-row items-center">
+                <GiScales className="w-10 h-10 text-green-700" />
+                <div className="flex flex-col ml-4 ">
+                  <h3 className="font-semibold">CLAT course</h3>
+                  <h5 className="text-sm">Browse all the CLAT courses</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div onClick={() => handleLibraryChange('CA Content')}>
+            <div className="bg-card rounded-md shadow-lg shadow-slate-400 h-24 mt-2 my-10 text-black flex items-center justify-start pl-4 mx-6 cursor-pointer">
+              <div className="flex flex-row items-center">
+                <FaRupeeSign className="w-10 h-10 text-yellow-700" />
+                <div className="flex flex-col ml-4 ">
+                  <h3 className="font-semibold">CA course</h3>
+                  <h5 className="text-sm">Browse all the CA courses</h5>
                 </div>
               </div>
             </div>
